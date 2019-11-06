cfd96668ab0813129adf1b577a52c3a578055046
5lue <wesley0940@gmail.com>
2019-11-07 03:03:35 +0900

    20191107

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import './App.css';
 function App(){
   return(
     <div className="root">
-      <header className="head">head</header>
-      <nav className="nav">nav</nav>
+      <header>
+        <div className="left">
+          <div className="about">About  </div>
+          <div className="store">Store</div>
+
+        </div>
+        <div className="right">
+          <div className="gmail">gmail</div>
+          <div className="image">image</div>
+        
+        </div>
+      </header>
+      <nav >
+      <div className="icon">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"></img>
+        </div>
+      </nav>
       <div>
-       <section>main</section>
-       <aside>aside</aside>
+       <section>
+         <div>
+         <div>
+          </div>
+         </div>
+         <div>
+           <input
+              type="text"
+              align="center"
+              placeholder="search"
+            />
+         </div>
+       
+       </section>
       </div>
       <footer>footer</footer>
     </div>
